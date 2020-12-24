abf92d619dcc9e15d8ae97c206e530de788c36b9
Abhishek Saxena <abhi_2611@hotmail.com>
2020-12-24 09:34:04 +0530

    Added my Comp and interpolation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MyCompComponent } from './my-comp/my-comp.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCompComponent
   ],
   imports: [
     BrowserModule,
